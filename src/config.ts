332be44ecda0ad7a30df4000e0503471d6d19a97
juan beresiarte <juanberesiarte@gmail.com>
2024-06-06 19:38:43 -0300

    Add portfolio

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export const SITE: Site = {
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 4,
-  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  scheduledPostMargin: 17 * 60 * 1000, // 15 minutes
 };
 
 export const LOCALE = {
@@ -48,100 +48,10 @@ export const SOCIALS: SocialObjects = [
     linkTitle: `Send an email to ${SITE.title}`,
     active: true,
   },
-  // {
-  //   name: "Facebook",
-  //   href: "https://github.com/satnaing/astro-paper",
-  //   linkTitle: `${SITE.title} on Facebook`,
-  //   active: false,
-  // },
-  // {
-  //   name: "Twitter",
-  //   href: "https://github.com/satnaing/astro-paper",
-  //   linkTitle: `${SITE.title} on Twitter`,
-  //   active: false,
-  // },
-  // {
-  //   name: "Twitch",
-  //   href: "https://github.com/satnaing/astro-paper",
-  //   linkTitle: `${SITE.title} on Twitch`,
-  //   active: false,
-  // },
-  // {
-  //   name: "YouTube",
-  //   href: "https://github.com/satnaing/astro-paper",
-  //   linkTitle: `${SITE.title} on YouTube`,
-  //   active: false,
-  // },
-  // {
-  //   name: "WhatsApp",
-  //   href: "https://github.com/satnaing/astro-paper",
-  //   linkTitle: `${SITE.title} on WhatsApp`,
-  //   active: false,
-  // },
-  // {
-  //   name: "Snapchat",
-  //   href: "https://github.com/satnaing/astro-paper",
-  //   linkTitle: `${SITE.title} on Snapchat`,
-  //   active: false,
-  // },
   {
     name: "Pinterest",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Pinterest`,
     active: false,
   },
-  // {
-  //   name: "TikTok",
-  //   href: "https://github.com/satnaing/astro-paper",
-  //   linkTitle: `${SITE.title} on TikTok`,
-  //   active: false,
-  // },
-  // {
-  //   name: "CodePen",
-  //   href: "https://github.com/satnaing/astro-paper",
-  //   linkTitle: `${SITE.title} on CodePen`,
-  //   active: false,
-  // },
-  // {
-  //   name: "Discord",
-  //   href: "https://github.com/satnaing/astro-paper",
-  //   linkTitle: `${SITE.title} on Discord`,
-  //   active: false,
-  // },
-  // {
-  //   name: "GitLab",
-  //   href: "https://github.com/satnaing/astro-paper",
-  //   linkTitle: `${SITE.title} on GitLab`,
-  //   active: false,
-  // },
-  // {
-  //   name: "Reddit",
-  //   href: "https://github.com/satnaing/astro-paper",
-  //   linkTitle: `${SITE.title} on Reddit`,
-  //   active: false,
-  // },
-  // {
-  //   name: "Skype",
-  //   href: "https://github.com/satnaing/astro-paper",
-  //   linkTitle: `${SITE.title} on Skype`,
-  //   active: false,
-  // },
-  // {
-  //   name: "Steam",
-  //   href: "https://github.com/satnaing/astro-paper",
-  //   linkTitle: `${SITE.title} on Steam`,
-  //   active: false,
-  // },
-  // {
-  //   name: "Telegram",
-  //   href: "https://github.com/satnaing/astro-paper",
-  //   linkTitle: `${SITE.title} on Telegram`,
-  //   active: false,
-  // },
-  // {
-  //   name: "Mastodon",
-  //   href: "https://github.com/satnaing/astro-paper",
-  //   linkTitle: `${SITE.title} on Mastodon`,
-  //   active: false,
-  // },
 ];
