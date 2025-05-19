22a895a4112e8a7327e14ab030911bd7d53d237f
juan beresiarte <juanberesiarte@gmail.com>
2025-05-19 10:10:54 -0300

    Update blog

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,11 +13,11 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
 
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
-    className: "text-xl font-medium decoration-dashed hover:underline",
+    className: "text-lg font-medium decoration-dashed hover:underline",
   };
 
   return (
-    <li className="my-6 text-left border-l-4 border-l-skin-accent pl-4 py-4 rounded-lg">
+    <li className="my-6 text-left border-l-8 border-l-skin-accent border-y-[1px] border-y-skin-accent border-r-2 border-r-skin-accent  pl-4 py-4 rounded-lg">
       <a
         href={href}
         className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
@@ -34,7 +34,7 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
         pubDatetime={pubDatetime}
         modDatetime={modDatetime}
       />
-      <p>{description}</p>
+      <p className="text-sm">{description}</p>
     </li>
   );
 }
